9f525f8f7143132ba9f57d54a73798c074cdf941
Vegard Stene <jvstene@gmail.com>
2014-02-12 14:25:45 +0100

    Make it simpler.

--- mediator.js
+++ mediator.js
@@ -1,9 +1,3 @@
 define(['underscore', 'backbone'], function(_, Backbone) {
-    var Mediator = {};
-
-    Mediator.subscribe = Backbone.Events.on;
-    Mediator.unsubscribe = Backbone.Events.off;
-    Mediator.publish = Backbone.Events.trigger;
-
-    return Mediator;
+    return _.clone(Backbone.Events);
 });
